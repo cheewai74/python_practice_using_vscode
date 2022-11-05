b5476f68007faa686d0df8f218c1665b34564629
Chee Wai Wong <academicsnotes@gmail.com>
2022-11-05 10:25:05 +0800

    Update mysql-csv-sales.py

--- Database/Code/mysql/mysql-csv-sales.py
+++ Database/Code/mysql/mysql-csv-sales.py
@@ -6,7 +6,9 @@ connection = mysql.connect(
     password='',
     host='localhost',
     database='sales',
-    allow_local_infile=True)
+    allow_local_infile=True)  
+
+# allow_local_infile to true let us read in our csv file locally
 
 cursor=connection.cursor()
     
@@ -22,13 +24,28 @@ create_query = '''CREATE TABLE salesperson(
 cursor.execute("DROP TABLE IF EXISTS salesperson")
 cursor.execute(create_query)
 
-with open('./salespeople.csv', 'r') as f:
-	csv_data = csv.reader(f)
-	for row in csv_data:
-		print(row)
-		row_tuple = tuple(row)
-		cursor.execute('INSERT INTO salesperson(first_name, last_name, email_address, city, state) VALUES("%s", "%s", "%s", "%s","%s")', row_tuple)
+# with open('./salespeople.csv', 'r') as f:
+# 	csv_data = csv.reader(f)
+# 	for row in csv_data:
+# 		print(row)
+# 		row_tuple = tuple(row)
+# 		cursor.execute('INSERT INTO salesperson(first_name, last_name, email_address, city, state) VALUES("%s", "%s", "%s", "%s","%s")', row_tuple)
         
+# Path whre you place your csv file in C:\work\mysql-csv-workspace
+       
+       
+# ---------------------------------------------------------------------------------------
+
+q = '''LOAD DATA LOCAL INFILE 
+'/work/mysql-csv-workspace/salespeople.csv'
+ INTO TABLE salesperson FIELDS TERMINATED BY ',' ENCLOSED BY '"' 
+ (first_name, last_name, email_address, city, state);'''
+
+
+cursor.execute(q)
+
+# ---------------------------------------------------------------------------------------
+
 connection.commit()
 cursor.execute("SELECT * FROM salesperson LIMIT 10")
 print(cursor.fetchall())

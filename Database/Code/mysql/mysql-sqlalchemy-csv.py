0e890e762ae5d691e2fab2a2fc3bc4ab3693b192
Chee Wai Wong <academicsnotes@gmail.com>
2022-11-05 20:46:29 +0800

    commit scripts.

---
+++ Database/Code/mysql/mysql-sqlalchemy-csv.py
@@ -0,0 +1,60 @@
+# 
+# 1. Create a folder C:\work\mysql-sqlalchemy-csv-workspace
+# 2. python -m venv C:\work\mysql-sqlalchemy-csv-workspace
+# 3. Copy python file to C:\work\mysql-sqlalchemy-csv-workspace
+# 4. Copy landon.csv to C:\work\mysql-sqlalchemy-csv-workspace
+#
+# pip install:
+# pip install pandas
+# pip install mysql-connector-python
+# pip install SQLAlchemy 
+
+import pandas
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('mysql+mysqlconnector://root:@localhost:3306/landon', 
+                       echo=True)
+
+Base = declarative_base()
+
+class Purchase(Base):
+	__tablename__ = 'purchases'
+	__table_args__ = {"schema":"landon"}
+
+	order_id = Column(Integer, primary_key=True)
+	property_id = Column(Integer)
+	property_city = Column(String(250))
+	property_state = Column(String(250))
+	product_id = Column(Integer)
+	product_category = Column(String(250))
+	product_name = Column(String(250))
+	quantity = Column(Integer)
+	product_price = Column(Float)
+	order_total = Column(Float)
+
+	def __repr__(self):
+		return '''<Purchase(order_id='{0}', property_id='{1}', 
+			property_city='{2}', property_state='{3}', 
+			product_id='{4}', product_category='{5}', 
+			product_name='{6}', quantity='{7}', product_price='{8}', 
+			order_total='{9}')>'''.format(self.order_id,
+			self.property_id, self.property_city,
+			self.property_state, self.product_id,
+			self.product_category, self.product_name,
+			self.quantity, self.product_price, self.order_total)
+
+Base.metadata.create_all(engine)
+
+file_name="landon.csv"
+df = pandas.read_csv(file_name) # data frame
+df.to_sql(con=engine, name=Purchase.__tablename__,if_exists='append', index=False)
+session = sessionmaker()
+session.configure(bind=engine)
+s=session()
+
+results = s.query(Purchase).imit(10).all()
+for r in results:
+    print(r)

872b2da7ce3689fb79b8c6c0f2b19e5b7c1ff420
Chee Wai Wong <academicsnotes@gmail.com>
2022-11-06 23:56:32 +0800

    Update postgres-db.py

--- Database/Code/postgre/postgres-db.py
+++ Database/Code/postgre/postgres-db.py
@@ -16,10 +16,11 @@
 
 import psycopg2
 conn = psycopg2.connect(database="red30",
-                        user='postgres',
-                        password="password",
-                        localhost="localhost",
-                        port="5432")
+	user="postgres",
+	password="password",
+	host="localhost",
+	port="5432")
+
 
 cursor = conn.cursor()
 

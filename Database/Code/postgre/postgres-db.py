176d6c9d849433f9ccfca142f6222efb938763b9
Chee Wai Wong <academicsnotes@gmail.com>
2022-11-06 23:16:12 +0800

    commit scripts

---
+++ Database/Code/postgre/postgres-db.py
@@ -0,0 +1,38 @@
+# 
+# 1. Create a folder C:\work\postgres-workspace
+# 2. python -m venv C:\work\postgres-workspace
+# 3. Copy python file to C:\work\postgres-workspace
+#
+# pip install:
+# pip install psycopg2
+# pip install psycopg2-binary
+#
+# Execute:
+# deactivate python virtual environment
+# psql -U postgres
+# \c red30
+# \dt
+
+
+import psycopg2
+conn = psycopg2.connect(database="red30",
+                        user='postgres',
+                        password="password",
+                        localhost="localhost",
+                        port="5432")
+
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE Sales
+    (ORDER_NUM INT PRIMARY KEY,
+        ORDER_TYPE TEXT,
+        CUST_NAME TEXT,
+        PROD_NUMBER TEXT,
+        PROD_NAME TEXT,
+        QUANTITY INT,
+        PRICE REAL,
+        DISCOUNT REAL,
+        ORDER_TOTAL REAL);''')
+
+conn.commit()
+conn.close()
---
+++ Database/Code/postgre/readMe.md
@@ -0,0 +1,14 @@
+initdb -D C:\work\postgreSQL
+
+cd C:\work\postgreSQL
+
+
+psql -U postgres
+\list
+CREATE DATABASE red30;
+\list 
+
+<!-- To connect to database red30 -->
+\c red30 
+
+C:\work\postgres-workspace

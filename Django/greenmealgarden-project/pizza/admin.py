86c4d14cea079298e598c5e729ebdf0889075104
Chee Wai Wong <academicsnotes@gmail.com>
2023-03-09 00:29:26 +0800

    commit scripts

--- Django/greenmealgarden-project/pizza/admin.py
+++ Django/greenmealgarden-project/pizza/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Pizza, Size
 
 # Register your models here.
+# Note: Any changes added here: 
+#       python manage.py makemigrations
+#       python manage.py migrate
+
+admin.site.register(Pizza)
+admin.site.register(Size) 

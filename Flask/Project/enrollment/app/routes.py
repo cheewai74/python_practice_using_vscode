36143e73c2d3d677c0845b5551f9a3088e29f62d
Chee Wai Wong <academicsnotes@gmail.com>
2023-03-19 00:44:23 +0800

    commit scripts

--- Flask/Project/enrollment/app/routes.py
+++ Flask/Project/enrollment/app/routes.py
@@ -1,5 +1,6 @@
-from app import app
-from flask import render_template
+import json
+from app import app, db
+from flask import Response, render_template, request
 
 # @app.route("/")
 # @app.route("/index")
@@ -10,20 +11,73 @@ from flask import render_template
 # from app import app
 # from flask import render_template
 
+courseData = [{"courseID":"1111","title":"PHP 101",
+                "description":"Intro to PHP","credits":3,
+                "term":"Fall, Spring"}, 
+                {"courseID":"2222","title":"Java 1",
+                "description":"Intro to Java Programming","credits":4,
+                "term":"Spring"}, 
+                {"courseID":"3333","title":"Adv PHP 201",
+                "description":"Advanced PHP Programming","credits":3,
+                "term":"Fall"}, 
+                {"courseID":"4444","title":"Angular 1",
+                "description":"Intro to Angular","credits":3,
+                "term":"Fall, Spring"}, 
+                {"courseID":"5555","title":"Java 2",
+                "description":"Advanced Java Programming","credits":4,
+                "term":"Fall"}]
+
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html", login=False)
+    return render_template("index.html", login=True)
 
 @app.route("/login")
 def login():
-    return render_template("login.html", login=False)
+    return render_template("login.html", login=True)
 
 @app.route("/courses")
-def courses():
-    return render_template("course.html", login=False)
+@app.route("/courses/<term>")
+def courses(term="2019"):
+    # print(courseData[0]["title"])
+    return render_template("courses.html", courseData=courseData, courses=True, term=term)
 
 @app.route("/register")
 def register():
-    return render_template("register.html", login=False)
+    return render_template("register.html", register=True)
+
+@app.route("/enrollment", methods=["GET", "POST"])
+def enrollment():
+    id = request.form.get('courseID')
+    title = request.form.get('title')
+    term = request.form.get('term')
+    return render_template("enrollment.html", 
+                           enrollment=True, 
+                           data={"id":id, 
+                                 "title":title, 
+                                 "term": term})
+    
+@app.route("/api/")
+@app.route("/api/<idx>")
+def api(idx=None):
+    if(idx == None):
+        jdata = courseData
+    else:
+        jdata = courseData[int(idx)]
+    return Response(json.dumps(jdata), mimetype="application/json")
+
+class User(db.Document):
+    user_id = db.IntField(unique=True)
+    first_name = db.StringField(max_length=50)
+    last_name = db.StringField(max_length=50)
+    email = db.StringField(max_length=30)
+    password = db.StringField(max_length=30)
+    
+@app.route("/user")
+def user():
+    # User(user_id=1, first_name="Zebra", last_name="Test", email="zebraswtestzsg711@gmail.com", password="zebratest123").save()
+    # User(user_id=2, first_name="Zebra", last_name="Test", email="zebraswtest12@hotmail.com", password="Zebratest123?").save()
+    users = User.objects.all()
+    return render_template("user.html", users=users)
+    
--- Flask/References/Readme.md
+++ Flask/References/Readme.md
@@ -7,6 +7,7 @@ PIP Install:
 pip install flask
 pip install flask-wtf
 pip install python-dotenv
+pip install flask-mongoengine
 pip list
 
 create a file ".flaskenv"
@@ -17,7 +18,10 @@ pip install -r requirements.txt
 Create a ".flaskenv" file.
 Create a file main.py
 Create a config.py
+Create a routes.py file in Application folder.
 
 
 flask commands:
 flask run
+
+mongo --version

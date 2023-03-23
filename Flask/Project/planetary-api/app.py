1d6a1128b633f6326aeed82a2be8a267366cf150
Chee Wai Wong <academicsnotes@gmail.com>
2023-03-24 00:26:28 +0800

    Commit scripts

--- Flask/Project/planetary-api/app.py
+++ Flask/Project/planetary-api/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/super_simple')
+def super_simple():
+    return jsonify(message='Hello from the Planetary API.')
+    
 
 if __name__ =='__main__':
     app.run()

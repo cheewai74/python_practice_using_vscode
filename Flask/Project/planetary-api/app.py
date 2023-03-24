a51b13537a76890e26d286109026a6fb3bd94a82
Chee Wai Wong <academicsnotes@gmail.com>
2023-03-24 11:54:23 +0800

    Commit scripts

--- Flask/Project/planetary-api/app.py
+++ Flask/Project/planetary-api/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, jsonify, request
+
+# SQLAlchemy Needed Import
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']
 
 @app.route('/')
 def hello_world():
@@ -8,7 +16,27 @@ def hello_world():
 
 @app.route('/super_simple')
 def super_simple():
-    return jsonify(message='Hello from the Planetary API.')
+    return jsonify(message='Hello from the Planetary API.'), 200
+    
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='That resource was not found.'), 404
+   
+@app.route('/parameters') 
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are old enough"), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough!"), 200
+        
+@app.route('/url_variables/<string:name>/<int:age>')    
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are old enough"), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough!"), 200
     
 
 if __name__ =='__main__':

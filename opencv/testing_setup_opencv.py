78b4eebac34d81907b25fb51fe27c914cbb97d0c
Chee Wai Wong <academicsnotes@gmail.com>
2024-11-03 09:32:40 +0800

    Commit opencv

--- opencv/testing_setup_opencv.py
+++ opencv/testing_setup_opencv.py
@@ -3,12 +3,14 @@ import cv2
 
 
 img = cv2.imread('doraemon.gif')
+cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
 cv2.imshow('image', img)
 
 
 # waits for user to press any key
 # (this is necessary to avoid Python kernel form crashing)
 cv2.waitKey(0)
+cv2.imwrite("output.jpg", img)
 
 # closing all open windows
 cv2.destroyAllWindows()
